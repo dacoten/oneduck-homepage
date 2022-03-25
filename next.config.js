87d5c2f1cb067f8f693b0755588fc6deab5b0999
oneduck <oneduck>
2022-03-25 18:30:44 +0900

    setup: font optimization (improve FCP and LCP)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    optimizeFonts: true,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
